6e29eaf70d5e883e6bed514a9b8b65d78d80dc62
Miss.Mathuja Parameshwaran <mathujaparameshwaran@gmail.com>
2026-03-17 10:55:19 +0530

    Delete .history directory

--- .history/backend/main_20260124221021.py
+++
@@ -1,4 +0,0 @@
-from network.tcp_server import start_master
-
-if __name__ == "__main__":
-    start_master()
--- .history/shared/message_types_20260209120533.py
+++
@@ -1,4 +0,0 @@
-REGISTER = "register"
-SCAN_TASK = "scan_task"
-SCAN_RESULT = "scan_result"
-DELETE_TASK = "delete_task"
